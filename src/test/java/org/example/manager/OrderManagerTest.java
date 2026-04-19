d2deb63c77d373ffb5877f31dcf8222026db5138
NickBezborodov <benlada1488@mail.ru>
2026-04-19 17:44:43 +0300

    Упорядочил InjectMocks. Убрал отступы. Добавил README

--- src/test/java/org/example/manager/OrderManagerTest.java
+++ src/test/java/org/example/manager/OrderManagerTest.java
@@ -10,52 +10,50 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.mockito.Mockito.*;
 
-    @ExtendWith(MockitoExtension.class)
+@ExtendWith(MockitoExtension.class)
+class OrderManagerTest {
 
-        class OrderManagerTest {
+    @Mock
+    private FileOrderReaderAdapter adapter;
+    @Mock
+    private OrderFileWriter writer;
+    @Mock
+    private OrderProcessor processor;
+    @Mock
+    private OrderReader reader;
+    @InjectMocks
+    private OrderManager manager;
 
-        @Mock
-        private FileOrderReaderAdapter adapter;
-        @Mock
-        private OrderFileWriter writer;
-        @Mock
-        private OrderProcessor processor;
-        @InjectMocks
-        private OrderManager manager;
-        @Mock
-        private OrderReader reader;
+    @Test
+    void processOrders_ShouldCallDependenciesInCorrectOrder() {
+        String inputFile = "input.txt";
+        String outputFile = "output.txt";
+        double pricePerKg = 10.0;
+        double startDiscount = 50.0;
+        double discountStep = 5.0;
 
-        @Test
-               void processOrders_ShouldCallDependenciesInCorrectOrder() {
-            String inputFile = "input.txt";
-            String outputFile = "output.txt";
-            double pricePerKg = 10.0;
-            double startDiscount = 50.0;
-            double discountStep = 5.0;
-
-            List<Order> mockOrders = List.of();
-            List<OrderResult> mockResult = List.of();
+        List<Order> mockOrders = List.of();
+        List<OrderResult> mockResult = List.of();
 
 
-            when(adapter.getReader(inputFile)).thenReturn(reader);
-            when(reader.readOrders(inputFile)).thenReturn(mockOrders);
-            when(processor.calculateCosts(mockOrders, pricePerKg, startDiscount, discountStep))
-                    .thenReturn(mockResult);
+        when(adapter.getReader(inputFile)).thenReturn(reader);
+        when(reader.readOrders(inputFile)).thenReturn(mockOrders);
+        when(processor.calculateCosts(mockOrders, pricePerKg, startDiscount, discountStep))
+                .thenReturn(mockResult);
 
-            manager.processOrders(inputFile, outputFile, pricePerKg, startDiscount, discountStep);
+        manager.processOrders(inputFile, outputFile, pricePerKg, startDiscount, discountStep);
 
-            verify(adapter).getReader(inputFile);
-            verify(reader).readOrders(inputFile);
-            verify(processor).calculateCosts(mockOrders, pricePerKg, startDiscount, discountStep);
-            verify(writer).writeResult(mockResult,outputFile);
-        }
+        verify(adapter).getReader(inputFile);
+        verify(reader).readOrders(inputFile);
+        verify(processor).calculateCosts(mockOrders, pricePerKg, startDiscount, discountStep);
+        verify(writer).writeResult(mockResult, outputFile);
     }
+}
 
 
