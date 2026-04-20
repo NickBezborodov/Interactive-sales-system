c31720cefce3794c7d91edadfcb49368e7dbbefb
NickBezborodov <benlada1488@mail.ru>
2026-04-20 22:34:16 +0300

    Добавил еще тесты.

--- src/test/java/org/example/processor/OrderProcessorTest.java
+++ src/test/java/org/example/processor/OrderProcessorTest.java
@@ -3,6 +3,7 @@ package org.example.processor;
 import org.example.model.Order;
 import org.example.model.OrderResult;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Or;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,25 +11,23 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class OrderProcessorTest {
+    private Order createOrder(int hour, String company, int quantity) {
+        LocalDateTime time = LocalDateTime.of(2024, 1, 1, hour, 0);
+        return new Order(time, company, quantity);
+    }
 
     @Test
     void discountCalculator() {
         LocalDateTime time1 = LocalDateTime.of(2024, 1, 1, 8, 0);
         LocalDateTime time2 = LocalDateTime.of(2024, 1, 1, 9, 0);
         LocalDateTime time3 = LocalDateTime.of(2024, 1, 1, 10, 0);
+        LocalDateTime time4 = LocalDateTime.of(2024, 1, 1, 11, 0);
 
         // Заказы
-        List<OrderResult> results = getOrderResults(time1, time2, time3);
-
-        assertEquals(5000.0, results.get(0).getFinalPrice());
-        assertEquals(2750.0, results.get(1).getFinalPrice());
-        assertEquals(1200.0, results.get(2).getFinalPrice());
-    }
-
-    private static List<OrderResult> getOrderResults(LocalDateTime time1, LocalDateTime time2, LocalDateTime time3) {
         Order order1 = new Order(time1, "First", 1000);
         Order order2 = new Order(time2, "Second", 500);
         Order order3 = new Order(time3, "Third", 200);
+
 
         // Список заказов
         List<Order> orders = List.of(order1, order2, order3);
@@ -40,7 +39,70 @@ public class OrderProcessorTest {
 
         OrderProcessor processor = new OrderProcessor();
         List<OrderResult> results = processor.calculateCosts(orders, 10.0, 50.0, 5.0);
-        return results;
+
+        assertEquals(5000.0, results.get(0).getFinalPrice());
+        assertEquals(2750.0, results.get(1).getFinalPrice());
+        assertEquals(1200.0, results.get(2).getFinalPrice());
+
     }
 
+    @Test
+    void shouldStopAtZero_WhenStartIs7AndStepIs3() {
+        Order order1 = createOrder(8, "First", 100);
+        Order order2 = createOrder(9, "Second", 100);
+        Order order3 = createOrder(10, "Third", 100);
+        Order order4 = createOrder(11, "Fourth", 100);
+
+        List<Order> orders = List.of(order1, order2, order3, order4);
+        OrderProcessor processor = new OrderProcessor();
+
+        List<OrderResult> results = processor.calculateCosts(orders, 10.0, 7.0, 3.0);
+
+        double[] expected = {929.9999999999999, 960.0, 990.0, 1000.0};
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals(expected[i], results.get(i).getFinalPrice());
+
+        }
+
+    }
+
+    @Test
+    void forAnEmptyList() {
+        List<Order> orders = List.of();
+
+        OrderProcessor processor = new OrderProcessor();
+
+        List<OrderResult> results = processor.calculateCosts(orders, 10.0, 50.0, 5.0);
+        assertTrue(results.isEmpty());
+    }
+
+    @Test
+    void priceToZero() {
+        Order order1 = createOrder(8, "First", 100);
+        Order order2 = createOrder(9, "Second", 200);
+        Order order3 = createOrder(10, "Third", 300);
+
+        List<Order> orders = List.of(order1, order2, order3);
+        OrderProcessor processor = new OrderProcessor();
+
+        List<OrderResult> results = processor.calculateCosts(orders, 0.0, 50.0, 5.0);
+        assertEquals(0.0, results.get(0).getFinalPrice());
+        assertEquals(0.0, results.get(1).getFinalPrice());
+        assertEquals(0.0, results.get(2).getFinalPrice());
+
+    }
+    @Test
+    void WhenStartDiscountIsZero(){
+        Order order1 = createOrder(8,"First", 100);
+        Order order2 = createOrder(9, "Second", 200);
+
+        List<Order> orders = List.of(order1, order2);
+        OrderProcessor processor = new OrderProcessor();
+
+        List<OrderResult> results = processor.calculateCosts(orders,10.0, 0.0, 5.0);
+
+        assertEquals(1000,results.get(0).getFinalPrice());
+        assertEquals(2000,results.get(1).getFinalPrice());
+    }
 }
+
